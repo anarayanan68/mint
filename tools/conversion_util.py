4e2d5890895b2ea01b720b40f4fe7f2953768103
Aravind Shankara Narayanan <anarayanan68@sky1.cc.gatech.edu>
2022-03-15 20:53:45 -0400

    6d conversion fns

--- tools/conversion_util.py
+++ tools/conversion_util.py
@@ -133,3 +133,41 @@ def get_closest_rotmat(rotmats):
   iden[..., 2, 2] = np.sign(det)
   r_closest = np.matmul(np.matmul(u, iden), vh)
   return r_closest.reshape(input_shape)
+
+
+def rotation_6d_to_matrix(d6: np.ndarray) -> np.ndarray:
+  """
+  Converts 6D rotation representation by Zhou et al. [1] to rotation matrix
+  using Gram--Schmidt orthogonalization per Section B of [1].
+  Args:
+      d6: 6D rotation representation, of size (*, 6)
+  Returns:
+      batch of rotation matrices of size (*, 3, 3)
+  [1] Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H.
+  On the Continuity of Rotation Representations in Neural Networks.
+  IEEE Conference on Computer Vision and Pattern Recognition, 2019.
+  Retrieved from http://arxiv.org/abs/1812.07035
+  """
+
+  a1, a2 = d6[..., :3], d6[..., 3:]
+  b1 = a1 / np.linalg.norm(a1, axis=-1, keepdims=True)
+  b2 = a2 - (b1 * a2).sum(-1, keepdims=True) * b1
+  b2 = b2 / np.linalg.norm(b2, axis=-1, keepdims=True)
+  b3 = np.cross(b1, b2, axis=-1)
+  return np.stack((b1, b2, b3), axis=-2)
+
+
+def matrix_to_rotation_6d(matrix: np.ndarray) -> np.ndarray:
+  """
+  Converts rotation matrices to 6D rotation representation by Zhou et al. [1]
+  by dropping the last row. Note that 6D representation is not unique.
+  Args:
+      matrix: batch of rotation matrices of size (*, 3, 3)
+  Returns:
+      6D rotation representation, of size (*, 6)
+  [1] Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H.
+  On the Continuity of Rotation Representations in Neural Networks.
+  IEEE Conference on Computer Vision and Pattern Recognition, 2019.
+  Retrieved from http://arxiv.org/abs/1812.07035
+  """
+  return np.copy(matrix[..., :2, :]).reshape(*matrix.shape[:-2], 6)
