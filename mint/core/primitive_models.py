c830706b4920911fc6ec796d8af8c6bd678684e8
Aravind Shankara Narayanan <anarayanan68@sky1.cc.gatech.edu>
2022-03-25 22:07:07 -0400

    next expt: L2 reg

--- mint/core/primitive_models.py
+++ mint/core/primitive_models.py
@@ -20,8 +20,9 @@ class Vec2SeqEncoder(keras.Model):
 
         prod_target_shape = np.prod(target_shape)
         initializer = keras.initializers.Constant(1.0 / np.sqrt(prod_target_shape)) # for unit output vector norm
+        regularizer = keras.regularizers.L2(l2=1e-3)
         self.embedding = layers.Embedding(num_primitives, prod_target_shape,
-            input_length=num_primitives, embeddings_initializer=initializer, name='embedding')
+            input_length=num_primitives, embeddings_initializer=initializer, embeddings_regularizer=regularizer, name='embedding')
 
         self.emb_input = tf.convert_to_tensor(np.arange(num_primitives))
         
