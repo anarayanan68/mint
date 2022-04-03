2db93f8bebfba160f7b564ea06f808e7d8222080
Aravind Shankara Narayanan <anarayanan68@sky1.cc.gatech.edu>
2022-04-03 13:26:41 -0400

    mesh-based visualization

--- tools/scratch_visualize.py
+++ tools/scratch_visualize.py
@@ -7,14 +7,15 @@ import glob
 import numpy as np
 from smplx import SMPL
 
-from visualization_util import load_motion, visualize_motion, compute_bbox, keypoints3D_from_axis_angles, axis_angles_from_6D
+from visualization_util import load_motion, visualize_motion
 
 
 # settings
 npy_path = sys.argv[1]
+viz_mode = sys.argv[2] if len(sys.argv) >= 3 else 'mesh'    # either 'mesh' or 'keypoints'
 num_frames = 60
 out_fps = 10
-bbox = ( np.array([0,2,0], dtype=np.float32), np.array([2.5,2.5,2.5], dtype=np.float32) )
+bbox = ( np.array([0,2,0], dtype=np.float32), np.array([2.5,2.5,2.5], dtype=np.float32) )   # computed before
 
 smpl_dir = '/home/aravind/work/hays-8903/store/smpl_models'
 gender = 'MALE'
@@ -24,13 +25,17 @@ smpl = SMPL(model_path=smpl_dir, gender=gender, batch_size=batch_size)
 
 if os.path.isfile(npy_path) and npy_path.endswith('.npy'):
     ## Uncomment to compute a bbox for reference
+    # from visualization_util import smpl_output_on_axis_angles, compute_bbox, axis_angles_from_6D
     # motion = load_motion(npy_path, num_frames)
-    # bb = compute_bbox(keypoints3D_from_axis_angles(*axis_angles_from_6D(motion), smpl))
+    # smpl_output = smpl_output_on_axis_angles(*axis_angles_from_6D(motion), smpl)
+    # bb = compute_bbox(smpl_output.joints.detach().numpy())
     # print(f'BBox computed: {bb}')
+
     out_vid_path = os.path.splitext(npy_path)[0] + ".mp4"
     visualize_motion(
         load_motion(npy_path, num_frames),
         smpl,
+        mode=viz_mode,
         bbox=bbox,
         out_video_path=out_vid_path,
         out_video_fps=out_fps
@@ -41,6 +46,7 @@ elif os.path.isdir(npy_path):
         visualize_motion(
             load_motion(file_path, num_frames),
             smpl,
+            mode=viz_mode,
             bbox=bbox,
             out_video_path=out_vid_path,
             out_video_fps=out_fps
--- tools/visualization_util.py
+++ tools/visualization_util.py
@@ -1,4 +1,5 @@
 import vedo
+import trimesh
 import torch
 import time
 import numpy as np
@@ -32,17 +33,16 @@ def axis_angles_from_6D(motions: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     return axis_angles, transl
 
 
-def keypoints3D_from_axis_angles(poses_1seq: np.ndarray, transl_1seq: np.ndarray, smpl_model: SMPL) -> np.ndarray:
+def smpl_output_on_axis_angles(poses_1seq: np.ndarray, transl_1seq: np.ndarray, smpl_model: SMPL):
     poses_1seq, transl_1seq = np.squeeze(poses_1seq), np.squeeze(transl_1seq)
     assert poses_1seq.ndim == 3     # 1 sequence only, shape (seq_len, 24, 3)
     assert transl_1seq.ndim == 2    # 1 sequence only, shape (seq_len, 3)
 
-    keypoints3d = smpl_model.forward(
+    return smpl_model.forward(
         global_orient=torch.from_numpy(poses_1seq[:, 0:1]).float(),
         body_pose=torch.from_numpy(poses_1seq[:, 1:]).float(),
         transl=torch.from_numpy(transl_1seq).float(),
-    ).joints.detach().numpy()
-    return keypoints3d
+    )
 
 
 def compute_bbox(keypoints3d: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
@@ -58,16 +58,29 @@ def compute_bbox(keypoints3d: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     return bbox_center, bbox_size
 
 
-def show_keypoint_seq(keypoints3d: np.ndarray, vedo_video: vedo.Video = None, bbox: Tuple[np.ndarray, np.ndarray] = None) -> None:
+def show_SMPL_output(smpl_output, smpl_model: SMPL,
+                     vedo_video: vedo.Video = None, mode: str = 'mesh', bbox: Tuple[np.ndarray, np.ndarray] = None) -> None:
+    keypoints3d = smpl_output.joints.detach().numpy()
     if bbox is None:
         bbox = compute_bbox(keypoints3d)
     bbox_center, bbox_size = bbox
-
     world = vedo.Box(bbox_center, bbox_size[0], bbox_size[1], bbox_size[2]).wireframe()
     plotter = vedo.show(world, axes=True, viewup="y", interactive=False)
-    for kpts in tqdm(keypoints3d):
-        pts = vedo.Points(kpts).c("red")
-        plotter = vedo.show(world, pts)
+
+    if mode == 'mesh':
+        vertices = smpl_output.vertices.detach().numpy()
+        faces = smpl_model.faces
+
+    num_frames = len(keypoints3d)
+    for i in range(num_frames):
+        pts = vedo.Points(keypoints3d[i]).c("red")
+        actors = [pts]
+        if mode == 'mesh':
+            mesh = trimesh.Trimesh(vertices[i], faces)
+            mesh.visual.face_colors = [200, 200, 250, 100]
+            actors.append(mesh)
+
+        plotter.show(world, *actors)
 
         if vedo_video is not None:
             vedo_video.addFrame()
@@ -75,6 +88,7 @@ def show_keypoint_seq(keypoints3d: np.ndarray, vedo_video: vedo.Video = None, bb
         if hasattr(plotter, 'escaped') and plotter.escaped:
             break  # if ESC
         time.sleep(0.01)
+
     # vedo.interactive().close()
     plotter.close()
     del world, plotter
@@ -82,15 +96,16 @@ def show_keypoint_seq(keypoints3d: np.ndarray, vedo_video: vedo.Video = None, bb
 
 def visualize_motion(   motion: np.ndarray,
                         smpl_model: SMPL,
+                        mode: str = 'mesh',
                         bbox: Tuple[np.ndarray, np.ndarray] = None,
                         out_video_path: str = None,
                         out_video_fps: int = 60) -> None:
     # TODO: handle multiple motions later if needed
     axis_angles, transl = axis_angles_from_6D(motion)
-    keypoints3d = keypoints3D_from_axis_angles(axis_angles, transl, smpl_model)
+    smpl_output = smpl_output_on_axis_angles(axis_angles, transl, smpl_model)
 
     vedo_video = (  vedo.Video(out_video_path, duration=None, fps=out_video_fps, backend='ffmpeg')
                     if out_video_path is not None
                     else None)
-    show_keypoint_seq(keypoints3d, vedo_video, bbox)
+    show_SMPL_output(smpl_output, smpl_model, vedo_video=vedo_video, mode=mode, bbox=bbox)
     vedo_video.close()
