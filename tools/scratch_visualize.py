bc253e6599b24a91e90ebfe3a3fbfbf674bee749
Aravind Shankara Narayanan <anarayanan68@sky1.cc.gatech.edu>
2022-05-02 19:33:28 -0400

    sync viz script changes

--- tools/scratch_visualize.py
+++ tools/scratch_visualize.py
@@ -12,7 +12,8 @@ from visualization_util import load_motion, visualize_motion
 
 # settings
 npy_path = sys.argv[1]
-viz_mode = sys.argv[2] if len(sys.argv) >= 3 else 'mesh'    # either 'mesh' or 'keypoints'
+fname_pattern = sys.argv[2] # e.g. 'OUTPUT--*'
+viz_mode = sys.argv[3] if len(sys.argv) >= 4 else 'mesh'    # either 'mesh' or 'keypoints'
 num_frames = 60
 out_fps = 10
 bbox = ( np.array([0,2,0], dtype=np.float32), np.array([2.5,2.5,2.5], dtype=np.float32) )   # computed before
@@ -41,15 +42,18 @@ if os.path.isfile(npy_path) and npy_path.endswith('.npy'):
         out_video_fps=out_fps
     )
 elif os.path.isdir(npy_path):
-    for file_path in glob.glob(os.path.join(npy_path, 'OUTPUT--*.npy'), recursive=True):
+    for file_path in sorted(glob.glob(os.path.join(npy_path, f'{fname_pattern}.npy'), recursive=True)):
         out_vid_path = os.path.splitext(file_path)[0] + ".mp4"
-        visualize_motion(
-            load_motion(file_path, num_frames),
-            smpl,
-            mode=viz_mode,
-            bbox=bbox,
-            out_video_path=out_vid_path,
-            out_video_fps=out_fps
-        )
+        if not os.path.exists(out_vid_path):
+            visualize_motion(
+                load_motion(file_path, num_frames),
+                smpl,
+                mode=viz_mode,
+                bbox=bbox,
+                out_video_path=out_vid_path,
+                out_video_fps=out_fps
+            )
+        else:
+            print(f"Skipping pre-existing out file: {out_vid_path}")
 else:
     print(f"INVALID PATH: {npy_path}")
