bcb80e4cd33e758350f4b7926dcd1a640db8ff94
Aravind Shankara Narayanan <anarayanan68@sky1.cc.gatech.edu>
2022-05-04 13:16:40 -0400

    docopt in scratch viz to make life easier

--- tools/scratch_visualize.py
+++ tools/scratch_visualize.py
@@ -1,6 +1,20 @@
-'''
-Scratchpad for visualization - modify as requirements change.
-'''
+"""Scratchpad for visualization - modify as requirements change. Doc for current form of usage below.
+
+Usage:
+    scratch_visualize.py VIZ_PATH [-p PATTERN -v VIZ_MODE --overwrite_existing --compute_bbox]
+
+Options:
+    VIZ_PATH                    Path to motion npy file, or folder containing motion npy files
+    -p, --pattern=PATTERN       Glob pattern for filenames (rooted at VIZ_PATH, without the extension). [default: *]
+                                For example, passing '**/*' will select all npy files in all subfolders of VIZ_PATH.
+                                Use quotes while passing this argument to avoid shell wildcard expansion.
+                                Only used if VIZ_PATH is a folder.
+    -v, --viz_mode=VIZ_MODE     Visualization mode: either 'mesh' or 'keypoints'. [default: mesh]
+    --overwrite_existing        Overwrite all pre-existing outputs. Default behavior is to skip all such files.
+                                Only used if VIZ_PATH is a folder.
+    --compute_bbox              Compute a tight bbox on the motion and show its values. Default behavior is to use the hard-coded bbox.
+                                Only used if VIZ_PATH is a file.
+"""
 import os
 import sys
 import glob
@@ -8,48 +22,59 @@ import numpy as np
 from smplx import SMPL
 
 from visualization_util import load_motion, visualize_motion
+from docopt import docopt
 
 
-# settings
-npy_path = sys.argv[1]
-viz_mode = sys.argv[2] if len(sys.argv) >= 3 else 'mesh'    # either 'mesh' or 'keypoints'
-num_frames = 60
-out_fps = 10
-bbox = ( np.array([0,2,0], dtype=np.float32), np.array([2.5,2.5,2.5], dtype=np.float32) )   # computed before
+if __name__ == '__main__':
+    args = docopt(__doc__)
 
-smpl_dir = '/home/aravind/work/hays-8903/store/smpl_models'
-gender = 'MALE'
-batch_size = 1
+    # settings
+    viz_path = args['VIZ_PATH']
+    fname_pattern = args['--pattern']
+    viz_mode = args['--viz_mode']
+    overwrite_existing = args['--overwrite_existing']
+    to_compute_bbox = args['--compute_bbox']
 
-smpl = SMPL(model_path=smpl_dir, gender=gender, batch_size=batch_size)
+    num_frames = 60
+    out_fps = 10
+    bbox = ( np.array([0,2,0], dtype=np.float32), np.array([2.5,2.5,2.5], dtype=np.float32) )   # computed before
 
-if os.path.isfile(npy_path) and npy_path.endswith('.npy'):
-    ## Uncomment to compute a bbox for reference
-    # from visualization_util import smpl_output_on_axis_angles, compute_bbox, axis_angles_from_6D
-    # motion = load_motion(npy_path, num_frames)
-    # smpl_output = smpl_output_on_axis_angles(*axis_angles_from_6D(motion), smpl)
-    # bb = compute_bbox(smpl_output.joints.detach().numpy())
-    # print(f'BBox computed: {bb}')
+    smpl_dir = '/home/aravind/work/hays-8903/store/smpl_models'
+    gender = 'MALE'
+    batch_size = 1
 
-    out_vid_path = os.path.splitext(npy_path)[0] + ".mp4"
-    visualize_motion(
-        load_motion(npy_path, num_frames),
-        smpl,
-        mode=viz_mode,
-        bbox=bbox,
-        out_video_path=out_vid_path,
-        out_video_fps=out_fps
-    )
-elif os.path.isdir(npy_path):
-    for file_path in glob.glob(os.path.join(npy_path, 'OUTPUT--*.npy'), recursive=True):
-        out_vid_path = os.path.splitext(file_path)[0] + ".mp4"
+    smpl = SMPL(model_path=smpl_dir, gender=gender, batch_size=batch_size)
+
+    if os.path.isfile(viz_path) and viz_path.endswith('.npy'):
+        if to_compute_bbox:
+            from visualization_util import smpl_output_on_axis_angles, compute_bbox, axis_angles_from_6D
+            motion = load_motion(viz_path, num_frames)
+            smpl_output = smpl_output_on_axis_angles(*axis_angles_from_6D(motion), smpl)
+            bb = compute_bbox(smpl_output.joints.detach().numpy())
+            print(f'BBox computed: {bb}')
+
+        out_vid_path = os.path.splitext(viz_path)[0] + ".mp4"
         visualize_motion(
-            load_motion(file_path, num_frames),
+            load_motion(viz_path, num_frames),
             smpl,
             mode=viz_mode,
             bbox=bbox,
             out_video_path=out_vid_path,
             out_video_fps=out_fps
         )
-else:
-    print(f"INVALID PATH: {npy_path}")
+    elif os.path.isdir(viz_path):
+        for file_path in sorted(glob.glob(os.path.join(viz_path, f'{fname_pattern}.npy'), recursive=True)):
+            out_vid_path = os.path.splitext(file_path)[0] + ".mp4"
+            if not os.path.exists(out_vid_path) or overwrite_existing:
+                visualize_motion(
+                    load_motion(file_path, num_frames),
+                    smpl,
+                    mode=viz_mode,
+                    bbox=bbox,
+                    out_video_path=out_vid_path,
+                    out_video_fps=out_fps
+                )
+            else:
+                print(f"Skipping pre-existing out file: {out_vid_path}")
+    else:
+        print(f"INVALID PATH: {viz_path}")
